f023327c04298834d6a350227424aef86eb1bd94
omarhaitham666 <oh2278943@gmail.com>
2026-03-11 07:11:38 +0200

    function is out the component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   let items = ["boston", "new york", "chicago", "los angelos"];
+  const handleOnSelection = (item:string)=>{
+    console.log(item);
+  }
   return (
     <>
-      <Message  items={items} heading='cities' onSelectItem={(item) => console.log(item)} />
+      <Message  items={items} heading='cities' onSelectItem={handleOnSelection} />
     </>
   )
 }
